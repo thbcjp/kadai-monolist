4cfe0d57136ff2cc92aef3587c1be8e64e3bf6a3
thbcjp <contact@thbc.jp>
2018-10-12 19:30:02 +0000

    ussr login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
         <div class="cover-inner">
             <div class="cover-content">
                 <h1>素敵になものと出会う場所</h1>
-                <a href="" class="btn btn-success btn-lg">モノリストを始める</a>
+                @if(!Auth::check())
+                <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">モノリストを始める</a>
+                @endif
             </div>
         </div>
     </div>
